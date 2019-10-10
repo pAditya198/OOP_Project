a18069d53beabb6b3fb748dcedfb8440169a2507
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2019-10-11 00:16:02 +0530

    added admin functionality

--- Admin.java
+++ Admin.java
@@ -3,22 +3,65 @@ import java.io.*;
 public class Admin
 {
 	static Scanner in=new Scanner(System.in);
-	private void writeNoOfStudents(int number,int batch)throws IOException,FileNotFoundException
+	private void writeNoOfStudents(int number,int batch)throws IOException
 	{
 		new File("iiitv\\student").mkdir();
 		new File("iiitv\\student\\"+batch).mkdir();
+		int newno = number+readNoOfStudents(batch);
 		FileOutputStream fout = new FileOutputStream("iiitv\\student\\"+batch+"\\Number.iiitv");
 		DataOutputStream d=new DataOutputStream(fout);
-		d.writeInt(number);
+		d.writeInt(newno);
 		d.close();
 		fout.close();
 	}
-	void assignRollNo(Students students[])throws IOException,FileNotFoundException,ClassNotFoundException
+	private int readNoOfStudents(int batch)throws IOException,FileNotFoundException
 	{
-		FileInputStream fin=new FileInputStream("iiitv\\student\\"+batch+"\\Number.iiitv");
-		DataInputStream f=new DataInputStream(fin);
-		int number=f.readInt();
-		System.out.println(number);
+		try
+		{
+			FileInputStream fin=new FileInputStream("iiitv\\student\\"+batch+"\\Number.iiitv");
+			DataInputStream r=new DataInputStream(fin);
+			int number=r.readInt();
+			r.close();
+			fin.close();
+			return number;
+		}
+		catch(FileNotFoundException E)
+		{
+			return 0;
+		}
+		catch(EOFException E)
+		{
+			return 0;
+		}
+	}
+	private void tempWriteStudent(Student obj,int index)throws IOException
+	{
+		new File("iiitv\\student").mkdir();
+		new File("iiitv\\student\\"+obj.getBatch()).mkdir();
+		new File("iiitv\\student\\"+obj.getBatch()+"\\"+index).mkdir();
+		FileOutputStream fout = new FileOutputStream("iiitv\\student\\"+obj.getBatch()+"\\"+index+"\\details.iiitv");
+		ObjectOutputStream oos = new ObjectOutputStream(fout);
+		oos.writeObject(obj);
+	}
+	private Student tempReadStudent(int batch,int index)throws IOException,FileNotFoundException,ClassNotFoundException
+	{
+		FileInputStream fin = new FileInputStream("iiitv\\student\\"+"\\"+batch+"\\"+index+"\\details.iiitv");
+		ObjectInputStream ois = new ObjectInputStream(fin);
+		Student obj=(Student)ois.readObject();
+		return obj;
+	}
+	void assignRollNo(int batch)throws IOException,FileNotFoundException,ClassNotFoundException
+	{
+		int number=readNoOfStudents(batch);
+		System.out.println("Error");
+		Student students[]=new Student[number];
+		for(int i=0;i<number;i++)
+		{
+			students[i]=tempReadStudent(batch,i);
+			students[i].readDetails(i);
+			System.out.println("Error\n\n");
+		}
+		
 		for(int i=0;i<number;i++)
 		{
 			for(int j=0;j<number-i-1;j++)
@@ -31,43 +74,44 @@ public class Admin
 				}
 			}
 		}
-		
+		writeRoll(students);
+	}
+	private void writeRoll(Student students[])throws IOException
+	{
 		int i=1;
 		for(Student t:students)
 		{
-			t.writeRollNo(batch*100000+i+51000);
+			t.writeRollNo(t.getBatch()*100000+i+51000);
 			i++;
 		}
 	}
-	private Student addBatch()
-	{
-		System.out.println();
-		System.out.print("Enter batch of Student: ");
-		int batch=in.nextInt();
-		return new Student(batch);
-	}
 	void displayStudents(int batch)throws IOException,ClassNotFoundException,FileNotFoundException
 	{
-		FileInputStream fin=new FileInputStream("iiitv\\student\\"+batch+"Number.iiitv");
-		int number=fin.read();
+		int number=readNoOfStudents(batch);
 		Student t=new Student(batch);
 		for(int i=0;i<number;i++)
-			t.readDetails(batch*100000+i+51001);
+			t.readDetails(i);
+		//t.readDetails(batch*100000+i+51001);
 	}
-	private int addStudent()
+	private Student addStudent(int batch)
 	{
-		Student obj=addBatch();
+		Student obj=new Student(batch);
 		obj.addDetails();
-		return obj
+		return obj;
 	}
 	int addStudents()throws IOException
 	{
 		System.out.print("Enter number of students: ");
 		int number=in.nextInt();
-		int batch=0;
-		Students students[]=new Students[number];
+		System.out.print("Enter batch of students: ");
+		int batch=in.nextInt();
+		int previous=readNoOfStudents(batch);
+		Student students[]=new Student[number];
 		for(int i=0;i<number;i++)
-			students[i]=addStudent();
+		{
+			students[i]=addStudent(batch);
+			tempWriteStudent(students[i],i+previous);
+		}
 		writeNoOfStudents(number,batch);
 		return batch;
 	}
--- Driver.java
+++ Driver.java
@@ -5,9 +5,9 @@ public class Driver
 	public static void main(String args[])throws IOException,ClassNotFoundException
 	{
 		Admin ob=new Admin();
-		int batch=ob.addStudents();
-		ob.assignRollNo(batch);
-		ob.displayStudents(batch);
+		//int batch=ob.addStudents();
+		ob.assignRollNo(2018);
+		//ob.displayStudents(2018);
 		//Student ob=new Student(2018);
 		//ob.readDetails(201851001);
 	}
