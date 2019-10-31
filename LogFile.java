cd46295d2fe47ccdf5d277a0eaf03374fafe1da7
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2019-10-31 19:18:14 +0530

    Added Professor class

---
+++ LogFile.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.time.*;
+public class LogFile<Type>
+{
+	Type ID;
+	String change;
+	LocalDateTime date;
+	LogFile(Type ID, String change,String change)
+	{
+		this.ID=ID;
+		this.change=change;
+		this.date=LocalDateTime.now();
+	}
+	void writeChange()
+	{
+		if (ID instanceof Student)
+		{
+			File f=new File("iiitv\\student\\"+ID.getBatch()+"\\"+ID.getRollNo()+"log.txt");
+			FileOutputStream ob=new FileOutputStream(f,true);
+	}
+}
