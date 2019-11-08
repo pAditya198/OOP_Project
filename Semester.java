5e214de25e678ce8ecdb918e6e39c74c74bc8fe3
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2019-11-09 01:27:52 +0530

    Updated semester

--- Semester.java
+++ Semester.java
@@ -1,42 +1,31 @@
 import java.util.*;
 import java.io.*;
 
-public class Semester {
+public class Semester 
+{
     static Scanner in = new Scanner(System.in);
-
-    public void writeSem1(String rollno) throws IOException {
-        String header = "";
-        FileReader csvr = null;
-        BufferedReader csv = null;
-        try {
-            new File("iiitv/Semester/Sem1").mkdirs();
-            csvr = new FileReader("iiitv/Semester/Sem1/SC101.csv");
-            csv = new BufferedReader(csvr);
-            header = csv.readLine();
-        } catch (FileNotFoundException e) {
-            header = "ID";
-        }
-        header += "\n";
-        String x;
-        List<String> atte = new ArrayList<String>();
-        while ((x = csv.readLine()) != null) {
-            x += "\n" + rollno;
-            atte.add(x);
-        }
-        csv.close();
-        csvr.close();
-        FileWriter cs = new FileWriter("iiitv/Semester/Sem1/SC101.csv");
-        cs.append(header);
-        for (String i : atte) {
-            System.out.println(i);
-            cs.append(i);
-        }
+    public void writeSemester(int rollNo, int sem,String subjectCode) throws IOException 
+	{    
+		FileWriter cs = null;
+		try
+		{
+			FileReader f=new FileReader("iiitv/Semester/"+sem+"/"+subjectCode+".csv");
+			cs=new FileWriter("iiitv/Semester/"+sem+"/"+subjectCode+".csv",true);
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("XYZ");
+			new File("iiitv/Semester/"+sem).mkdirs();
+			cs=new FileWriter("iiitv/Semester/"+sem+"/"+subjectCode+".csv",true);
+			cs.append("ID,\n");
+		}
+		cs.append(rollNo+",\n");
         cs.flush();
         cs.close();
     }
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) throws IOException 
+	{
         Semester ob = new Semester();
-        ob.writeSem1("201851008");
+        ob.writeSemester(201851018,1,"CS201");
     }
 }
