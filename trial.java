6c9dd401c7f92c3be5acdead27e5e6415fc13464
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2019-11-13 01:21:52 +0530

    Changed trial

--- trial.java
+++ trial.java
@@ -9,7 +9,7 @@ public class trial
         *///ob.writeNoOfStudents(2, 2019);
         //System.out.println(ob.readNoOfStudents(2019));
         AttendanceWriter ob=new AttendanceWriter();
-        ob.writeAttendance(3, "CS201");
+        ob.openAttendance(3, "CS201");
         //ob.writeAttendance(3, "SC201");
     }
 }
