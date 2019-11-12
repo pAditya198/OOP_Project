0dbc1e818817c4cf82f54f98da69e4ce06af7ca7
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2019-11-12 23:38:02 +0530

    Added excel opener

--- trial.java
+++ trial.java
@@ -2,10 +2,12 @@ public class trial
 {
     public static void main(String args[])
     {
-        AdminModule ob = new AdminModule();
-        ob.assignRollNo(2018);
+        //AdminModule ob = new AdminModule();
+        //ob.assignRollNo(2018);
         //ob.writeNoOfStudents(2, 2019);
         //System.out.println(ob.readNoOfStudents(2019));
-
+        AttendanceWriter ob=new AttendanceWriter();
+        ob.writeAttendance(3, "CS201");
+        ob.writeAttendance(3, "SC201");
     }
 }
