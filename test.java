eb17b3aa8f0717e8e79f55e98c9e0b914a13e073
Prateek9144 <prateekchouhan00@gmail.com>
2019-11-17 18:52:21 +0530

    some chages

--- test.java
+++ test.java
@@ -9,17 +9,19 @@ package gui;
  *
  * @author prate
  */
-public class AdminGUI extends javax.swing.JFrame {
+
+    import java.io.*;
+public class AdminStudentRegistration extends javax.swing.JFrame {
 
     /**
-     * Creates new form AdminGUI
+     * Creates new form AdminStudentRegistration
      */
-    public AdminGUI() {
+    public AdminStudentRegistration() {
+        
         initComponents();
-       setLocationRelativeTo(null);
-
+          setLocationRelativeTo(null);
+      
     }
-    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -31,175 +33,70 @@ public class AdminGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        StudentRegistration = new javax.swing.JLabel();
-        ProfessorRegistrataion = new javax.swing.JLabel();
-        Mail = new javax.swing.JLabel();
-        EditStudentProfile = new javax.swing.JLabel();
-        Assign = new javax.swing.JLabel();
-        Marks = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setAutoRequestFocus(false);
-        setLocation(new java.awt.Point(0, 0));
-        setPreferredSize(new java.awt.Dimension(800, 400));
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setLocation(new java.awt.Point(600, 400));
         setResizable(false);
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
 
-        jPanel2.setBackground(new java.awt.Color(0, 0, 255));
+        jLabel1.setFont(new java.awt.Font("Trebuchet MS", 1, 14)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Batch :");
 
-        jLabel7.setFont(new java.awt.Font("Georgia", 0, 18)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel7.setText("Admin Profile");
+        jLabel2.setFont(new java.awt.Font("Trebuchet MS", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Number of Student :");
 
-        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/gui/folder/270x270-male-avatar.png"))); // NOI18N
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(29, 29, 29)
-                        .addComponent(jLabel8))
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGap(73, 73, 73)
-                        .addComponent(jLabel7)))
-                .addContainerGap(43, Short.MAX_VALUE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(68, Short.MAX_VALUE)
-                .addComponent(jLabel8)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel7)
-                .addGap(95, 95, 95))
-        );
-
-        StudentRegistration.setBackground(new java.awt.Color(0, 0, 153));
-        StudentRegistration.setFont(new java.awt.Font("Segoe UI Historic", 1, 14)); // NOI18N
-        StudentRegistration.setForeground(new java.awt.Color(255, 255, 255));
-        StudentRegistration.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        StudentRegistration.setText("Student Registration");
-        StudentRegistration.setToolTipText("");
-        StudentRegistration.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        StudentRegistration.setOpaque(true);
-        StudentRegistration.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                StudentRegistrationMouseClicked(evt);
+        jButton1.setText("Next");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
             }
         });
 
-        ProfessorRegistrataion.setBackground(new java.awt.Color(0, 0, 153));
-        ProfessorRegistrataion.setFont(new java.awt.Font("Segoe UI Historic", 1, 14)); // NOI18N
-        ProfessorRegistrataion.setForeground(new java.awt.Color(255, 255, 255));
-        ProfessorRegistrataion.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        ProfessorRegistrataion.setText("Professor Registration");
-        ProfessorRegistrataion.setToolTipText("");
-        ProfessorRegistrataion.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        ProfessorRegistrataion.setOpaque(true);
-        ProfessorRegistrataion.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                ProfessorRegistrataionMouseClicked(evt);
-            }
-        });
+        jTextField1.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
 
-        Mail.setBackground(new java.awt.Color(0, 0, 153));
-        Mail.setFont(new java.awt.Font("Segoe UI Historic", 1, 14)); // NOI18N
-        Mail.setForeground(new java.awt.Color(255, 255, 255));
-        Mail.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        Mail.setText("Mail Defaulters");
-        Mail.setToolTipText("");
-        Mail.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        Mail.setOpaque(true);
-        Mail.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                MailMouseClicked(evt);
-            }
-        });
-
-        EditStudentProfile.setBackground(new java.awt.Color(0, 0, 153));
-        EditStudentProfile.setFont(new java.awt.Font("Segoe UI Historic", 1, 14)); // NOI18N
-        EditStudentProfile.setForeground(new java.awt.Color(255, 255, 255));
-        EditStudentProfile.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        EditStudentProfile.setText("Edit Student Profile");
-        EditStudentProfile.setToolTipText("");
-        EditStudentProfile.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        EditStudentProfile.setOpaque(true);
-        EditStudentProfile.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                EditStudentProfileMouseClicked(evt);
-            }
-        });
-
-        Assign.setBackground(new java.awt.Color(0, 0, 153));
-        Assign.setFont(new java.awt.Font("Segoe UI Historic", 1, 14)); // NOI18N
-        Assign.setForeground(new java.awt.Color(255, 255, 255));
-        Assign.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        Assign.setText("Assign Roll Numbers");
-        Assign.setToolTipText("");
-        Assign.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        Assign.setOpaque(true);
-        Assign.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                AssignMouseClicked(evt);
-            }
-        });
-
-        Marks.setBackground(new java.awt.Color(0, 0, 153));
-        Marks.setFont(new java.awt.Font("Segoe UI Historic", 1, 14)); // NOI18N
-        Marks.setForeground(new java.awt.Color(255, 255, 255));
-        Marks.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        Marks.setText("Open Marks Sheet");
-        Marks.setToolTipText("");
-        Marks.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        Marks.setOpaque(true);
-        Marks.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                MarksMouseClicked(evt);
-            }
-        });
+        jTextField2.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(59, 59, 59)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(StudentRegistration, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Mail, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Assign, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(ProfessorRegistrataion, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(EditStudentProfile, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Marks, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(49, 49, 49))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(62, 62, 62)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(168, 168, 168)
+                        .addComponent(jButton1)))
+                .addContainerGap(145, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(60, 60, 60)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(StudentRegistration, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(ProfessorRegistrataion, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(44, 44, 44)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Mail, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(EditStudentProfile, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(45, 45, 45)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Assign, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Marks, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(26, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addGap(50, 50, 50))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -216,29 +113,14 @@ public class AdminGUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>                        
 
-    private void StudentRegistrationMouseClicked(java.awt.event.MouseEvent evt) {                                                 
-        // TODO add your handling code here:
-    }                                                
-
-    private void ProfessorRegistrataionMouseClicked(java.awt.event.MouseEvent evt) {                                                    
-        // TODO add your handling code here:
-    }                                                   
-
-    private void MailMouseClicked(java.awt.event.MouseEvent evt) {                                  
-        // TODO add your handling code here:
-    }                                 
-
-    private void EditStudentProfileMouseClicked(java.awt.event.MouseEvent evt) {                                                
-        // TODO add your handling code here:
-    }                                               
-
-    private void AssignMouseClicked(java.awt.event.MouseEvent evt) {                                    
-        // TODO add your handling code here:
-    }                                   
-
-    private void MarksMouseClicked(java.awt.event.MouseEvent evt) {                                   
-        // TODO add your handling code here:
-    }                                  
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+  AdminModule ob = new AdminModule();
+        int batch = Integer.parseInt(jTextField1.getText());
+        int number = Integer.parseInt(jTextField2.getText());
+            ob.addStudents(number, batch);
+        dispose();
+       
+    }                                        
 
     /**
      * @param args the command line arguments
@@ -257,34 +139,30 @@ public class AdminGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AdminGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminStudentRegistration.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AdminGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminStudentRegistration.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AdminGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminStudentRegistration.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AdminGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminStudentRegistration.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AdminGUI().setVisible(true);
+                new AdminStudentRegistration().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify                     
-    private javax.swing.JLabel Assign;
-    private javax.swing.JLabel EditStudentProfile;
-    private javax.swing.JLabel Mail;
-    private javax.swing.JLabel Marks;
-    private javax.swing.JLabel ProfessorRegistrataion;
-    private javax.swing.JLabel StudentRegistration;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration                   
 }
--- trial.java
+++ trial.java
@@ -2,8 +2,8 @@ public class trial
 {
     public static void main(String args[])
     {
-        //AdminModule ob = new AdminModule();
-        //ob.assignRollNo(2018);
+        // AdminModule ob = new AdminModule();
+        // ob.assignRollNo(2018);
         //ob.writeNoOfStudents(2, 2019);
         /*AdminModule ob = new AdminModule();
         ob.deleteFolder("iiitv\\student\\"+2018+"\\1");
@@ -11,14 +11,16 @@ public class trial
         ob.deleteFolder("iiitv\\student\\"+2018+"\\3");
         *///ob.writeNoOfStudents(2, 2019);
         //System.out.println(ob.readNoOfStudents(2019));
-        AttendanceWriter ob=new AttendanceWriter();
-        ob.writeAttendance(3, "CS201");
+        // AttendanceWriter ob=new AttendanceWriter();
+        // ob.openAttendance(3, "CS201");
         // AttendanceWriter ob=new AttendanceWriter();
         // ob.writeAttendance(3, "CS201");
         //ob.writeAttendance(3, "SC201");
-        //Professor ob = new Professor();
+        // Professor ob = new Professor();
         // ProfessorRegistration obj = new ProfessorRegistration(ob);
         // obj.setVisible(true);
-        //ob.readDetails(1818);
+        //ob.readDetails(1299);
+        AdminStudentRegistration ob = new AdminStudentRegistration();
+        ob.setVisible(true);
     }
 }
