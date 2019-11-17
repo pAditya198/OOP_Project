05db90c2e3baf7dca9d153ba713c56def99d8670
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2019-11-17 16:03:34 +0530

    Changes in login

--- LoginPage.java
+++ LoginPage.java
@@ -126,7 +126,6 @@ public class LoginPage extends javax.swing.JFrame {
                 .addContainerGap(48, Short.MAX_VALUE))
         );
 
-        jLabel5.setText("Login Page");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -164,7 +163,7 @@ public class LoginPage extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-
+        setTitle("Login Page");
         pack();
     }// </editor-fold>
 
